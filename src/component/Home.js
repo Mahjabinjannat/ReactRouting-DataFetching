b049a6e2bb4df50f166fa7e56f694d1fc0e21128
Mahjabin Jannat <mahjabinjannat153@gmail.com>
2024-03-04 16:29:48 +0600

    Homepage Modified

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", padding: "20px", margin: "10px" }}>
+        Welcome to home
+      </h1>
+      <p style={{ textAlign: "center", padding: "0px 200px" }}>
+        The -m option may be used to supplement the log message of the created
+        commit, but the additional commentary will be thrown away once the
+        "fixup!" commit is squashed into by git rebase --autosquash . The commit
+        created by --fixup=amend: is similar but its subject is instead prefixed
+        with "amend!".
+      </p>
+    </div>
+  );
 }
 
 export default Home;
